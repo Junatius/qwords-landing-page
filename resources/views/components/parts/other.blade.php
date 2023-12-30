4f1869a858f1733b1227b4a25766d70fb914ccbf
Junatius <cjuniar0@gmail.com>
2023-12-31 05:26:51 +0800

    fix: adjust the review view to carousel

--- resources/views/components/parts/other.blade.php
+++ resources/views/components/parts/other.blade.php
@@ -1,6 +1,6 @@
 <section id="testimonial">
 	<div class="container-fluid">
-		<div class="row justify-content-center align-items-center">
+		<div class="row justify-content-center align-items-center pb-4">
 			<div class="col-12 col-lg-6 text-center">
 				<h2>Apa Kata Mereka?</h2>
 				<p>
@@ -8,96 +8,114 @@
 				</p>
 			</div>
 		</div>
-		<div class="row justify-content-center align-items-center mb-5 mb-lg-3 from-left">
-			<div class="order-2 order-lg-1 col-12 col-lg-6">
-				<div class="header-quote">
-					<h5>
-						Semoga Qwords dapat terus bertahan, sehingga kami dan masyarakat Indonesia bisa mendapatkan layanan yang terbaik dari Qwords.
-					</h5>
+		<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-inner">
+			  <div class="carousel-item active">
+				<div class="row justify-content-center align-items-center mb-5 mb-lg-3 from-left">
+					<div class="order-2 order-lg-1 col-12 col-lg-6">
+						<div class="header-quote">
+							<h5>
+								Semoga Qwords dapat terus bertahan, sehingga kami dan masyarakat Indonesia bisa mendapatkan layanan yang terbaik dari Qwords.
+							</h5>
+						</div>
+						<div class="quote-details">
+							<p class="fst-italic">
+								Kami, ID Cert, merupakan sebuah perusahaan yang bergerak dalam bidang penyedia jasa informasi keamanan di Indonesia. Oleh
+								karena itu, kami membutuhkan Qwords untuk mendukung kebutuhan kami terhadap server dan hosting yang selalu up selama 24 jam.
+							</p>
+						</div>
+						<div class="author-name ms-5">
+							<h6 class="d-inline me-3">Budi Raharjo</h6>
+							<span class="small text-muted">Founder ID Cert</span>
+						</div>
+						<div class="mt-2">
+							<a href="https://youtu.be/q2bJogin3eE" class="text-qw-primary ms-5 me-2 text-decoration-none fw-bold my-3">Lihat Video Selengkapnya
+								<img data-lazyloaded="1" data-placeholder-resp="20pxx20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMHAiIGhlaWdodD0iIiB2aWV3Qm94PSIwIDAgMjBwICI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4=" loading="lazy" class="ms-3" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /><noscript><img loading="lazy" class="ms-3" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /></noscript>
+							</a>
+						</div>
+					</div>
+					<div class="order-1 order-lg-2 col-12 order-lg-2 col-lg-6 mb-3 mb-lg-5">
+						<div class="img-wrapper mx-auto rounded-4 p-3">
+							<img data-lazyloaded="1" data-placeholder-resp="100%x100%" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxMDAlIDEwMCUiPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHN0eWxlPSJmaWxsOiNjZmQ0ZGI7ZmlsbC1vcGFjaXR5OiAwLjE7Ii8+PC9zdmc+" loading="lazy" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi1.webp" alt="testi1.webp" width="100%" height="100%"><noscript><img loading="lazy" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi1.webp" alt="testi1.webp" width="100%" height="100%"></noscript>
+						</div>
+					</div>
 				</div>
-				<div class="quote-details">
-					<p class="fst-italic">
-						Kami, ID Cert, merupakan sebuah perusahaan yang bergerak dalam bidang penyedia jasa informasi keamanan di Indonesia. Oleh
-						karena itu, kami membutuhkan Qwords untuk mendukung kebutuhan kami terhadap server dan hosting yang selalu up selama 24 jam.
-					</p>
+			  </div>
+			  <div class="carousel-item">
+				<div class="row justify-content-center align-items-center mb-5 mb-lg-3 from-left">
+					<div class="order-2 order-lg-2 col-12 col-lg-6">
+						<div class="header-quote">
+							<h5>
+								Pelayanan Qwords sangat cepat dan responsif, sehingga tidak butuh waktu lama untuk membantu website kami kembali normal.
+								Terbaik pokoknya, mah!
+							</h5>
+						</div>
+						<div class="quote-details">
+							<p class="fst-italic">
+								Sebagai media penyedia kabar seputar kota Bandung, Info BDG menggunakan website sebagai rumah informasi. Website kami yang
+								sempat down kini menunjukkan traffic yang tinggi setelah menggunakan dukungan dari Qwords.
+							</p>
+						</div>
+						<div class="author-name ms-5">
+							<h6 class="d-inline me-3">Kuns Kurniawan</h6>
+							<span class="small text-muted">PR Event Info BDG</span>
+						</div>
+						<div class="mt-2">
+							<a href="https://youtu.be/f15zt4C2e4A" class="text-qw-primary ms-5 me-2 text-decoration-none fw-bold my-3">Lihat Video Selengkapnya
+								<img data-lazyloaded="1" data-placeholder-resp="20pxx20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMHAiIGhlaWdodD0iIiB2aWV3Qm94PSIwIDAgMjBwICI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4=" loading="lazy" class="ms-3" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /><noscript><img loading="lazy" class="ms-3" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /></noscript>
+							</a>
+						</div>
+					</div>
+					<div class="order-1 order-lg-1 col-12 col-lg-6">
+						<div class="img-wrapper mx-auto rounded-4 p-3">
+							<img data-lazyloaded="1" data-placeholder-resp="100%x100%" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxMDAlIDEwMCUiPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHN0eWxlPSJmaWxsOiNjZmQ0ZGI7ZmlsbC1vcGFjaXR5OiAwLjE7Ii8+PC9zdmc+" loading="lazy" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi2.webp" alt="testi2.webp" width="100%" height="100%"><noscript><img loading="lazy" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi2.webp" alt="testi2.webp" width="100%" height="100%"></noscript>
+						</div>
+					</div>
 				</div>
-				<div class="author-name ms-5">
-					<h6 class="d-inline me-3">Budi Raharjo</h6>
-					<span class="small text-muted">Founder ID Cert</span>
-				</div>
-				<div class="mt-2">
-					<a href="https://youtu.be/q2bJogin3eE" class="text-qw-primary ms-5 me-2 text-decoration-none fw-bold my-3">Lihat Video Selengkapnya
-						<img data-lazyloaded="1" data-placeholder-resp="20pxx20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMHAiIGhlaWdodD0iIiB2aWV3Qm94PSIwIDAgMjBwICI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4=" loading="lazy" class="ms-3" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /><noscript><img loading="lazy" class="ms-3" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /></noscript>
-					</a>
+			  </div>
+			  <div class="carousel-item">
+				<div class="row justify-content-center align-items-center mb-5 mb-lg-3 from-left">
+					<div class="order-2 order-lg-1 col-12 col-lg-6">
+						<div class="header-quote">
+							<h5>
+								Hingga saat ini pula, kami tidak pernah menemukan masalah yang berarti dari layanan yang diberikan Qwords. Layanan Customer
+								Service yang sangat baik, membuat kami tidak ingin berpindah dari Qwords.
+							</h5>
+						</div>
+						<div class="quote-details">
+							<p class="fst-italic">
+								Penjualan Rumah Kopi Temanggung 90% dilakukan secara online, salah satunya menggunakan website. Semenjak tahun 2015 hingga
+								saat ini, kami menggunakan hosting dan domain dari Qwords.
+							</p>
+						</div>
+						<div class="author-name ms-5">
+							<h6 class="d-inline me-3">Denden Sofiudin</h6>
+							<span class="small text-muted">Owner Rumah Kopi temanggung</span>
+						</div>
+						<div class="mt-2">
+							<a href="https://youtu.be/ftJMF63RznM" class="text-qw-primary ms-5 me-2 text-decoration-none fw-bold my-3">Lihat Video Selengkapnya
+								<img data-lazyloaded="1" data-placeholder-resp="20pxx20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMHAiIGhlaWdodD0iIiB2aWV3Qm94PSIwIDAgMjBwICI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4=" loading="lazy" class="ms-3" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /><noscript><img loading="lazy" class="ms-3" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /></noscript>
+							</a>
+						</div>
+					</div>
+					<div class="order-1 order-lg-2 col-12 col-lg-6">
+						<div class="img-wrapper mx-auto rounded-4 p-3">
+							<img data-lazyloaded="1" data-placeholder-resp="100%x100%" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxMDAlIDEwMCUiPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHN0eWxlPSJmaWxsOiNjZmQ0ZGI7ZmlsbC1vcGFjaXR5OiAwLjE7Ii8+PC9zdmc+" loading="lazy" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi3.webp" alt="testi3.webp" width="100%" height="100%"><noscript><img loading="lazy" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi3.webp" alt="testi3.webp" width="100%" height="100%"></noscript>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="order-1 order-lg-2 col-12 order-lg-2 col-lg-6 mb-3 mb-lg-5">
-				<div class="img-wrapper mx-auto rounded-4 p-3">
-					<img data-lazyloaded="1" data-placeholder-resp="100%x100%" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxMDAlIDEwMCUiPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHN0eWxlPSJmaWxsOiNjZmQ0ZGI7ZmlsbC1vcGFjaXR5OiAwLjE7Ii8+PC9zdmc+" loading="lazy" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi1.webp" alt="testi1.webp" width="100%" height="100%"><noscript><img loading="lazy" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi1.webp" alt="testi1.webp" width="100%" height="100%"></noscript>
-				</div>
+			  </div>
 			</div>
-		</div>
-		<div class="row justify-content-center align-items-center mb-5 mb-lg-3 from-left">
-			<div class="order-2 order-lg-2 col-12 col-lg-6">
-				<div class="header-quote">
-					<h5>
-						Pelayanan Qwords sangat cepat dan responsif, sehingga tidak butuh waktu lama untuk membantu website kami kembali normal.
-						Terbaik pokoknya, mah!
-					</h5>
-				</div>
-				<div class="quote-details">
-					<p class="fst-italic">
-						Sebagai media penyedia kabar seputar kota Bandung, Info BDG menggunakan website sebagai rumah informasi. Website kami yang
-						sempat down kini menunjukkan traffic yang tinggi setelah menggunakan dukungan dari Qwords.
-					</p>
-				</div>
-				<div class="author-name ms-5">
-					<h6 class="d-inline me-3">Kuns Kurniawan</h6>
-					<span class="small text-muted">PR Event Info BDG</span>
-				</div>
-				<div class="mt-2">
-					<a href="https://youtu.be/f15zt4C2e4A" class="text-qw-primary ms-5 me-2 text-decoration-none fw-bold my-3">Lihat Video Selengkapnya
-						<img data-lazyloaded="1" data-placeholder-resp="20pxx20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMHAiIGhlaWdodD0iIiB2aWV3Qm94PSIwIDAgMjBwICI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4=" loading="lazy" class="ms-3" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /><noscript><img loading="lazy" class="ms-3" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /></noscript>
-					</a>
-				</div>
-			</div>
-			<div class="order-1 order-lg-1 col-12 col-lg-6">
-				<div class="img-wrapper mx-auto rounded-4 p-3">
-					<img data-lazyloaded="1" data-placeholder-resp="100%x100%" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxMDAlIDEwMCUiPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHN0eWxlPSJmaWxsOiNjZmQ0ZGI7ZmlsbC1vcGFjaXR5OiAwLjE7Ii8+PC9zdmc+" loading="lazy" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi2.webp" alt="testi2.webp" width="100%" height="100%"><noscript><img loading="lazy" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi2.webp" alt="testi2.webp" width="100%" height="100%"></noscript>
-				</div>
-			</div>
-		</div>
-		<div class="row justify-content-center align-items-center mb-5 mb-lg-3 from-left">
-			<div class="order-2 order-lg-1 col-12 col-lg-6">
-				<div class="header-quote">
-					<h5>
-						Hingga saat ini pula, kami tidak pernah menemukan masalah yang berarti dari layanan yang diberikan Qwords. Layanan Customer
-						Service yang sangat baik, membuat kami tidak ingin berpindah dari Qwords.
-					</h5>
-				</div>
-				<div class="quote-details">
-					<p class="fst-italic">
-						Penjualan Rumah Kopi Temanggung 90% dilakukan secara online, salah satunya menggunakan website. Semenjak tahun 2015 hingga
-						saat ini, kami menggunakan hosting dan domain dari Qwords.
-					</p>
-				</div>
-				<div class="author-name ms-5">
-					<h6 class="d-inline me-3">Denden Sofiudin</h6>
-					<span class="small text-muted">Owner Rumah Kopi temanggung</span>
-				</div>
-				<div class="mt-2">
-					<a href="https://youtu.be/ftJMF63RznM" class="text-qw-primary ms-5 me-2 text-decoration-none fw-bold my-3">Lihat Video Selengkapnya
-						<img data-lazyloaded="1" data-placeholder-resp="20pxx20px" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMHAiIGhlaWdodD0iIiB2aWV3Qm94PSIwIDAgMjBwICI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4=" loading="lazy" class="ms-3" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /><noscript><img loading="lazy" class="ms-3" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/Right.webp" alt="Right.webp" width="20px" height="20px" /></noscript>
-					</a>
-				</div>
-			</div>
-			<div class="order-1 order-lg-2 col-12 col-lg-6">
-				<div class="img-wrapper mx-auto rounded-4 p-3">
-					<img data-lazyloaded="1" data-placeholder-resp="100%x100%" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAxMDAlIDEwMCUiPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHN0eWxlPSJmaWxsOiNjZmQ0ZGI7ZmlsbC1vcGFjaXR5OiAwLjE7Ii8+PC9zdmc+" loading="lazy" data-src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi3.webp" alt="testi3.webp" width="100%" height="100%"><noscript><img loading="lazy" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/photos/testi3.webp" alt="testi3.webp" width="100%" height="100%"></noscript>
-				</div>
-			</div>
-		</div>
-	</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+				<span data-feather="chevron-left" class="arrow" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			  </button>
+			  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+				<span data-feather="chevron-right" class="arrow" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			  </button>
+		  </div>
 </section>
 <section id="covered">
 	<div class="container-fluid">
